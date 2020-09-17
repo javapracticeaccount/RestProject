5fa03f39cb86617e21d24b29b592d03484e2f671
Mahesh Bompilwar <mahesh.bompilwar2@gmail.com>
2020-09-17 11:50:53 +0530

    GetMapping changes Made...

--- src/main/java/com/techdenovo/RestProject/ui/controller/UserController.java
+++ src/main/java/com/techdenovo/RestProject/ui/controller/UserController.java
@@ -3,29 +3,28 @@ package com.techdenovo.RestProject.ui.controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("users")
 public class UserController {
 
 
-    @GetMapping
+    @GetMapping("/")
     public String getUser(){
 
         return "Get user details..";
     }
 
-    @PostMapping
+    @PostMapping("/createUser")
     public String createUser(){
 
         return "create user..";
     }
 
-    @PutMapping
+    @PutMapping("/updateUser")
     public String updateUser(){
 
         return "Update user details..";
     }
 
-    @DeleteMapping
+    @DeleteMapping("/deleteUser")
     public String deleteUser(){
 
         return "delete user details..";
